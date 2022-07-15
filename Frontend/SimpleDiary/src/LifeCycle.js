47dc85629110240c6ecb6a2b261e97a1d9c719e9
Eomjihee <eomjihee.code@gmail.com>
2022-07-15 17:54:56 +0900

    6-7. unmount시 컨트롤 방법 숙지

--- Frontend/SimpleDiary/src/LifeCycle.js
+++ Frontend/SimpleDiary/src/LifeCycle.js
@@ -1,37 +1,21 @@
 import React, { useState, useEffect } from "react";
-
-const LifeCycle = () => {
-  const [cnt, setCnt] = useState(0);
-  const [text, setText] = useState('');
+const UnmountTest = () => {
   useEffect(()=> {
     console.log('[mount]');
-  },[])
-  /*
-  useEffect(()=> {
-    console.log('[update]');
-  })
-  */
- // 의존 배열을 잘 활용하면 감지하고 싶은 데이터만 골라 변경 감지하여 업데이트 처리 가능
-  useEffect(()=> {
-    // 배열의 값이 바뀌는 순간 콜백함수 호출
-    console.log(`[cnt update] : ${cnt}`);
-    if(cnt > 5){
-      alert('값이 5를 초과하여 초기화');
-      setCnt(0)
+    return () => {
+      // unmount 시점에만 실행
+      console.log('[unmount]');
     }
-  },[cnt])
-  useEffect(()=> {
-    console.log(`[text update] : ${text}`);
-  },[text])
+  }, [])
+  return <div>Unmount Testing Component</div>
+}
 
+const LifeCycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleIsVisible = () => setIsVisible(!isVisible);
   return <div style={{ padding: 20 }}>
-    <div>
-      {cnt}
-      <button onClick={() => setCnt(cnt+1)}>+</button>
-    </div>
-    <div>
-      <input value={text} onChange={e => setText(e.target.value)}/>
-    </div>
+    <button onClick={toggleIsVisible}>{isVisible ? 'OFF' : 'ON'}</button>
+    {isVisible && <UnmountTest/>}
   </div>;
 };
 
